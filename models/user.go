dd938b089d65679306fe5dea5999ab8b7e6cc658
ziya <ziyashaik2209@gmail.com>
2025-04-06 13:06:03 +0530

    refactor user model and account handler to use CreateUserRequest DTO for user creation

--- models/user.go
+++ models/user.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct {
-	Id       int
-	Name     string
-	Age      int
-	Email    string
-	Password string
-	Bio      string
+	Id           int
+	Username     string
+	Age          int
+	Email        string
+	PasswordHash string
+	Bio          string
 }
