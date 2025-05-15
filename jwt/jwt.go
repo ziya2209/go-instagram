a38f45fd3c657ebde1272db0853523d9f26d9dff
ziya2209 <ziyashaik2209@gmail.com>
2025-05-15 21:17:54 +0530

    feat: refactor NewToken function to accept username and return token string; add GetUsernameFromToken function for extracting username from token

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -10,9 +10,41 @@ import (
 
 
 var secretKey = "secret"
-func NewToken() {
-}
+func NewToken(username string) (string, error) {
+	// Create a new token object
+	token := jwt.New(jwt.SigningMethodHS256)
 
+	// Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = username
+
+	// Generate encoded token and send it as response
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+func GetUsernameFromToken(tokenString string) (string, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Ensure the signing method is HMAC
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, http.ErrAbortHandler
+		}
+		return []byte(secretKey), nil
+	})
+	if err != nil || !token.Valid {
+		return "", err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if username, ok := claims["username"].(string); ok {
+			return username, nil
+		}
+	}
+	return "", http.ErrAbortHandler
+}
 
 func VerifyToken(tokenString string) (bool, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
